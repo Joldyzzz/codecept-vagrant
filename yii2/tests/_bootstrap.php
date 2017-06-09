4b15f890a7c5884ec16e9eef12a830ce31dfaced
Гельметдинов Ильдус <joldyzzz@mail.ru>
2017-06-09 15:49:49 +0300

    coverage settings and include c3.php

--- yii2/tests/_bootstrap.php
+++ yii2/tests/_bootstrap.php
@@ -2,5 +2,7 @@
 define('YII_ENV', 'test');
 defined('YII_DEBUG') or define('YII_DEBUG', true);
 
+include __DIR__ . '/../c3.php';
+
 require_once(__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
 require __DIR__ . '/../vendor/autoload.php';
